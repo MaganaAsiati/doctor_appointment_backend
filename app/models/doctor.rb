cf71c2c7d4023be9b46f9ccd57bbd9947f2bd515
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-16 16:48:20 -0400

    add validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,11 @@
 class Doctor < ApplicationRecord
+
+    validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50'}
+    validates :speciality, presence: true, length: {in: 3..100, message: 'Speciality\'s length should be in range of 3 to 100'}
+    validates :image, presence: true
+    validates :reserved, presence: true
+    validates :bill, presence: true
+    validates :email, presence: true
+    validates :location, presence: true, length: {in: 2..100, message: 'Location\'s length should be in range of 2 to 100'}
+    validates :description, presence: true, numericality: true
 end
