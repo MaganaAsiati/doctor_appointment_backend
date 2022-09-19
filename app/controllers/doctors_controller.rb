30155c135c2a4c5c2cf6e358dca8b49ef98c2f39
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-19 11:40:36 -0400

    Created destroy, get_doctor and update method and add status to reponses

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,11 +1,46 @@
 class DoctorsController < ApplicationController
+    before_action :get_doctor, except: %i[create index]
+
     def index
         doctors = Doctor.all
-        render json: doctors
+        render json: doctors, status: :ok
     end
 
     def show
-        doctor = Doctor.find(params[:id])
-        render json: doctor
+        render json: @doctor, status: :ok
+    end
+
+    def create
+        doctor = Doctor.new(doctor_params)
+        if doctor.save
+            render json: doctor, status: :ok
+        else
+            render json: { errors: doctor.errors.full_messages },
+                    status: :unprocessable_entity
+        end
+    end
+
+    def update
+        unless @doctor.update(user_params)
+            render json: { errors: @doctor.errors.full_messages },
+                    status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @doctor.destroy
+    end
+
+    private
+
+    def doctor_params
+        params.permit(:name, :speciality, :image, :reserved, :description, :bill, :email, :location)
+    end
+
+    def get_doctor
+        @doctor = Doctor.find(id: params[:id])
+        rescue ActiveRecord::RecordNotFound
+            render json: { errors: 'Doctor not found' }, status: :not_found
+        end
     end
 end
