da71818e039694958a5d265fa92cb41f39668ca1
MaganaAsiati <assia.magana@gmail.com>
2022-09-19 18:14:21 +0200

    add path for reservation and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reservations
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
