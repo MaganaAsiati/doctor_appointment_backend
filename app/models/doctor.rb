ca2a58161cb92cb612924dfb3e3f571590af19b8
MaganaAsiati <assia.magana@gmail.com>
2022-09-19 18:34:52 +0200

    fix linters

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,12 +1,14 @@
 class Doctor < ApplicationRecord
-    has_many :reservations
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-    validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50'}
-    validates :speciality, presence: true, length: {in: 3..100, message: 'Speciality\'s length should be in range of 3 to 100'}
-    validates :image, presence: true
-    validates :reserved, presence: true
-    validates :bill, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :location, presence: true, length: {in: 2..100, message: 'Location\'s length should be in range of 2 to 100'}
-    validates :description, presence: true, numericality: true
+  has_many :reservations
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50' }
+  validates :speciality, presence: true,
+                         length: { in: 3..100, message: 'Speciality\'s length should be in range of 3 to 100' }
+  validates :image, presence: true
+  validates :reserved, presence: true
+  validates :bill, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :location, presence: true,
+                       length: { in: 2..100, message: 'Location\'s length should be in range of 2 to 100' }
+  validates :description, presence: true, numericality: true
 end
