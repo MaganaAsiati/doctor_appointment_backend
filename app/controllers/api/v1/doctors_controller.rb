c18fa9d68c4c4301303bfaf8057f0a0b998cc1bb
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-19 13:33:29 -0400

    text and fixed linter errors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,45 +1,47 @@
 class Api::V1::DoctorsController < ApplicationController
-    before_action :get_doctor, except: %i[create index]
+  before_action :found_doctor, except: %i[create index]
 
-    def index
-        doctors = Doctor.all
-        render json: doctors, status: :ok
+  def index
+    doctors = Doctor.all
+    render json: doctors, status: :ok
+  end
+
+  def show
+    render json: @doctor, status: :ok
+  end
+
+  def create
+    doctor = Doctor.new(doctor_params)
+    if doctor.save
+      render json: doctor, status: :ok
+    else
+      render json: { errors: doctor.errors.full_messages },
+             status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @doctor, status: :ok
+  def update
+    if @doctor.update(doctor_params)
+      render json: 'Doctor update successfully'
+    else
+      render json: { errors: @doctor.errors.full_messages },
+             status: :unprocessable_entity
     end
+  end
 
-    def create
-        doctor = Doctor.new(doctor_params)
-        if doctor.save
-            render json: doctor, status: :ok
-        else
-            render json: { errors: doctor.errors.full_messages },
-                    status: :unprocessable_entity
-        end
-    end
+  def destroy
+    render json: "#{@doctor.name} deleted sucessfully" if @doctor.destroy
+  end
 
-    def update
-        unless @doctor.update(user_params)
-            render json: { errors: @doctor.errors.full_messages },
-                    status: :unprocessable_entity
-        end
-    end
+  private
 
-    def destroy
-        @doctor.destroy
-    end
+  def doctor_params
+    params.permit(:name, :speciality, :image, :reserved, :description, :bill, :email, :location)
+  end
 
-    private
-
-    def doctor_params
-        params.permit(:name, :speciality, :image, :reserved, :description, :bill, :email, :location)
-    end
-
-    def get_doctor
-        @doctor = Doctor.find(id: params[:id])
-        rescue ActiveRecord::RecordNotFound
-            render json: { errors: 'Doctor not found' }, status: :not_found
-    end
+  def found_doctor
+    @doctor = Doctor.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'Doctor not found' }, status: :not_found
+  end
 end
--- test/controllers/doctors_controller_test.rb
+++ test/controllers/doctors_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class DoctorsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
