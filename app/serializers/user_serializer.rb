29d296e5a9191a87bea2e28be4fcd52d4f28664f
MaganaAsiati <assia.magana@gmail.com>
2022-09-20 17:05:02 +0200

    fix linter

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email
-  has_many :reservations
-  has_many :doctors
-  has_many :doctors_reservations, through: :reservations, source: :user
+  attributes :id, :name, :email, :role 
+  # has_many :reservations
+  # has_many :doctors
 end
