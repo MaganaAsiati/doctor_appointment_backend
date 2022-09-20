8973675fc19bc38587f4d0ca6046306189f442fc
MaganaAsiati <assia.magana@gmail.com>
2022-09-20 17:01:45 +0200

    update  serialize

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true, uniqueness: true
-  
 end
