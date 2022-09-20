10859cfc391a4d5d2fa08a66f38b8db4d41446af
MaganaAsiati <assia.magana@gmail.com>
2022-09-20 11:26:26 +0200

    add validations for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :reservations
   has_many :doctors
   has_many :doctors_reservations, through: :reservations, source: :user
 
   validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, uniqueness: true
+  
 end
