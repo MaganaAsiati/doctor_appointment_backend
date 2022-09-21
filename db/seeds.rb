ff85da8fa7ecf43a4d3431037c28bab16e3cd1b2
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-21 08:07:13 -0400

    add 2 doctors to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,26 @@
 # users
 User.create(name: 'User 1', email: 'user1@gmail.com', password: '123456')
 User.create(name: 'admin', email: 'admin@gamil.com', password: '123456', role: 'admin')
+
+# doctors
+Doctor.create(
+    name: 'John abou',
+    speciality: 'Neurology', 
+    image: 'https://img.freepik.com/free-photo/pleased-young-female-doctor-wearing-medical-robe-stethoscope-around-neck-standing-with-closed-posture_409827-254.jpg?w=2000',
+    reserved: true,
+    description: 'some description about this John abou',
+    bill: 100,
+    email: 'johnabou@gmail.com',
+    location: 'Cotonou'
+)
+
+Doctor.create(
+    name: 'Robert Yonou',
+    speciality: 'Neurology', 
+    image: 'https://media.istockphoto.com/photos/happy-healthcare-practitioner-picture-id138205019?k=20&m=138205019&s=612x612&w=0&h=KpsSMVsplkOqTnAJmOye4y6DcciVYIBe5dYDgYXLVW4=',
+    reserved: true,
+    description: 'Who is Robert yonou, I don\'t know now',
+    bill: 100,
+    email: 'robertyonou@gmail.com',
+    location: 'New York'
+)
