cbd06a1e9ba9f14053183c862dcc37243b25180a
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-24 22:32:12 -0400

    implement update_reserved method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :doctor, class_name: 'Doctor', foreign_key: 'doctor_id'
+  
+  after_save :update_reserved
+  after_destroy :update_reserved
+  
+  def update_reserved
+    if doctor.reservations.is_empty?
+      doctor.update(reserved: false) if doctor.reserved
+    else
+      doctor.update(reserved: true) unless doctor.reserved
+    end
+  end
 end
