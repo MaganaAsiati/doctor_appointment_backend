ae3023bc39768bfeb0a3de0c1c3529e4bf69d1b4
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-24 22:05:40 -0400

    add dependent to doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,11 +1,11 @@
 class Doctor < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
   validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50' }
   validates :speciality, presence: true,
                          length: { in: 3..100, message: 'Speciality\'s length should be in range of 3 to 100' }
   validates :image, presence: true
-  validates :reserved, presence: true
   validates :bill, presence: true
   validates :email, presence: true, uniqueness: true
   validates :location, presence: true,
