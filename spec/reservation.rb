9fad71942b9d60d8cafc1441714a64b50f298b92
MaganaAsiati <assia.magana@gmail.com>
2022-09-26 13:15:52 +0200

    add test for reservations request

---
+++ spec/reservation.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Reservation', type: :request do
+  user = User.create(name: 'User 1', email: 'user1@gmail.com', password: '123456')
+  Christina  = admin.doctors.create(
+    name: 'Christina Milian',
+    speciality: 'Neurology', 
+    image: 'https://user-images.githubusercontent.com/95297251/192105518-81e2d20a-4990-470a-98a2-eb57c4242d07.png',
+    description: 'some description about this Christina Milian',
+    bill: 100,
+    email: 'johnabou@gmail.com',
+    location: 'Cotonou'
+)
+user.reservations.create(date_reserved: '22-10-2022', city: 'Lome', duration: 20, doctor: Christina)
+  it 'should return the reservation' do
+    expect(reservation.doctor).to eq(doctor)
+  end
+
