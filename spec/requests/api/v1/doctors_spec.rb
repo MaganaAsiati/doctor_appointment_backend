c6311477ee8ac9c0b5686421a013bd960dc420ad
MaganaAsiati <assia.magana@gmail.com>
2022-09-26 13:16:36 +0200

    add test for doctors

--- spec/requests/api/v1/doctors_spec.rb
+++ spec/requests/api/v1/doctors_spec.rb
@@ -1,88 +1,83 @@
 require 'swagger_helper'
 
-# t.string "name", limit: 50
-# t.string "speciality", limit: 100
-# t.string "image"
-# t.boolean "reserved"
-# t.text "description"
-# t.decimal "bill"
-# t.string "email"
-# t.string "location", limit: 100
-
 
 RSpec.describe 'api/v1/doctors', type: :request do
-
   path '/api/v1/doctors' do
-
     get('list doctors') do
       tags 'Doctors'
       produces 'application/json'
-      
-      response(200, 'successful') do
+      security [Bearer: {}]
+      parameter name: :Authorization, in: :header, type: :string
+
+      response('200', 'successful') do
         schema type: :object
-        run_test!
+        let(:user) {User.create(name: 'User 1', email: 'user1@gmail.com', password: '123456')}
+        let(:token) {JWT.encode({ user_id: user.id }, 'asia', 'HS256')}
+        let(:Authorization) {token}
+        run_test! do |response|
+          puts user
+        end
       end
     end
 
-    post('create doctor') do
-      tags 'Doctors'
-      consumes 'application/json'
+      post('create doctor') do
+        tags 'Doctors'
+        consumes 'application/json'
 
+        response(200, 'successful') do
+          parameter name: :doctor, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            speciality: { type: :string }
+          },
+          required: [ 'title', 'content' ]
+        }
+
+        response '201', 'blog created' do
+          let(:blog) { { title: 'foo', content: 'bar' } }
+          run_test!
+        end
+
+        response '422', 'invalid request' do
+          let(:blog) { { title: 'foo' } }
+          run_test!
+        end
+      end
+    end
+
+    path '/api/v1/doctors/{id}' do
+      # You'll want to customize the parameter types...
+      parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show doctor') do
       response(200, 'successful') do
-        parameter name: :doctor, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          speciality: { type: :string }
-        },
-        required: [ 'title', 'content' ]
-      }
+        let(:id) { '123' }
 
-      response '201', 'blog created' do
-        let(:blog) { { title: 'foo', content: 'bar' } }
-        run_test!
-      end
-
-      response '422', 'invalid request' do
-        let(:blog) { { title: 'foo' } }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
         run_test!
       end
     end
-  end
 
-  path '/api/v1/doctors/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
+    delete('delete doctor') do
+      response(200, 'successful') do
+        let(:id) { '123' }
 
-    # get('show doctor') do
-    #   response(200, 'successful') do
-    #     let(:id) { '123' }
-
-    #     after do |example|
-    #       example.metadata[:response][:content] = {
-    #         'application/json' => {
-    #           example: JSON.parse(response.body, symbolize_names: true)
-    #         }
-    #       }
-    #     end
-    #     run_test!
-    #   end
-    # end
-
-
-    # delete('delete doctor') do
-    #   response(200, 'successful') do
-    #     let(:id) { '123' }
-
-    #     after do |example|
-    #       example.metadata[:response][:content] = {
-    #         'application/json' => {
-    #           example: JSON.parse(response.body, symbolize_names: true)
-    #         }
-    #       }
-    #     end
-    #     run_test!
-    #   end
-    # end
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
